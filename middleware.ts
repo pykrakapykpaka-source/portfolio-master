016e66c208c822d5b64023ec465f667d88c63d80
pawelwessel <wesiudev.rekrutacja@gmail.com>
2026-02-19 16:59:47 +0100

    3d components separated from the server

--- middleware.ts
+++ middleware.ts
@@ -6,16 +6,20 @@ import { i18n } from "./i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
-function getLocale(request: NextRequest): string | undefined {
-  // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+function getLocale(request: NextRequest): string {
+  try {
+    const negotiatorHeaders: Record<string, string> = {};
+    request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-  // Use negotiator and intl-localematcher to get best locale
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales;
-  return matchLocale(languages, locales, i18n.defaultLocale);
+    const languages = new Negotiator({
+      headers: negotiatorHeaders,
+    }).languages();
+    // @ts-ignore locales are readonly
+    const locales: string[] = i18n.locales;
+    return matchLocale(languages, locales, i18n.defaultLocale);
+  } catch {
+    return i18n.defaultLocale;
+  }
 }
 
 export function middleware(request: NextRequest) {
@@ -50,9 +54,7 @@ export function middleware(request: NextRequest) {
 
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
-    return NextResponse.redirect(
-      new URL(`/${locale}${pathname}`, request.url)
-    );
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
   }
 }
 
